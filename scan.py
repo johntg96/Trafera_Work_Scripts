d457c8ccd3814bf87a8056bb740a9a6e388dcd7c
John Garrison <johntg@protonmail.com>
2021-12-11 16:13:57 -0600

    first commit

---
+++ scan.py
@@ -0,0 +1,26 @@
+import sys
+import pyperclip
+
+print('|||||--Trafera Work Scripts--|||||')
+def menu():
+    choice = int(input('\n1: Scan SRO\n2: Exit\n\n'))
+
+    if choice == 1:
+        scan()
+    elif choice == 2:
+        sys.exit()
+    else:
+        print('Invalid choice')
+        menu()
+
+def scan():
+    raw_sro = input('\nScan SRO:\n\n')
+    if raw_sro == '~':
+        sys.exit()
+    else:
+        legit_sro = raw_sro.replace('R', 'S').replace('r', 'S').replace('P', 'R').replace('p', 'R').replace('Y', 'O').replace('y', 'O')
+        pyperclip.copy(legit_sro)
+        print('\n"' + pyperclip.paste() + '" copied to clipboard!')
+        scan()
+
+menu()
