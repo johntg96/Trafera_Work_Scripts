71dcdcc1821d28b3440db0beddd633ffb94b801e
John Garrison <johntg@protonmail.com>
2022-01-09 20:43:27 -0600

    Detects scan KB layout and acts accordingly. Adds accepted scans to list using new  DailyStats() which offers much room to grow.

--- scan.py
+++ scan.py
@@ -1,27 +1,59 @@
 import sys
 import re
+import time
 import pyperclip
 
-print('|||||--Trafera Work Scripts--|||||')
+print('|||||--Trafera Work Script--|||||')
+
+sro_list = []
+
+def split(word):
+    return [char for char in word]
+
+def dailyStats():
+    print(sro_list)
+    num_units = len(sro_list)
+    return "Total # of SRO's scanned: " + str(num_units)
+
 def menu():
-    choice = int(input('\n1: Scan SRO\n2: Exit\n\n'))
+    choice = int(input('\n1: Scan SRO\n2: Daily Stats\n3: Exit\n\n'))
 
     if choice == 1:
         scan()
     elif choice == 2:
-        sys.exit()
+        print(dailyStats())
+    elif choice == 3:
+        sys.exit(0)
     else:
         print('Invalid choice')
         menu()
 
 def scan():
+    global sro_list
     raw_sro = input('\nScan SRO:\n\n')
-    if raw_sro == '~':
-        sys.exit()
-    else:
-        legit_sro = raw_sro.replace('R', 'S').replace('r', 'S').replace('P', 'R').replace('p', 'R').replace('Y', 'O').replace('y', 'O').replace('	','')
-        pyperclip.copy(legit_sro)
-        print('\n"' + pyperclip.paste() + '" copied to clipboard!')
+    raw_sro.upper()
+    raw_sro_list = split(raw_sro)
+
+    #check if SRO has been scanned in using Colemak-Mod DH layout or not
+    if raw_sro_list[0]+raw_sro_list[1]+raw_sro_list[2] == "RPY":
+        mod_sro = raw_sro.replace('R', 'S').replace('P', 'R').replace('Y', 'O').replace('	','')
+        sro_list.append(mod_sro)
+        pyperclip.copy(mod_sro)
+    elif raw_sro_list[0]+raw_sro_list[1]+raw_sro_list[2] == "SRO":
+        sro_list.append(raw_sro)
+        pyperclip.copy(raw_sro)
+    elif raw_sro == 'menu':
+        menu()
+    elif raw_sro == "daily":
+        print(dailyStats())
         scan()
+    elif raw_sro == 'exit':
+        sys.exit(0)
+    else:
+        print("SRO not detected, try again.")
+        scan()
+
+    print('\n' + pyperclip.paste() + ' copied to clipboard')
+    scan()
 
 menu()
