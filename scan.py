0348e6e3abc72fc22f71e003ff7e5bfb06e35e8e
John Garrison <johntg@protonmail.com>
2022-01-11 21:25:14 -0600

    Minor grammatical changes and quotation syntax changes for consistency.

--- scan.py
+++ scan.py
@@ -30,15 +30,15 @@ def dailyStats():
     return "Total # of unique SRO's scanned: " + str(num_units)
 
 def menu():
-    choice = input('\n1: Scan SRO\n2: Daily Stats\n3: Scan SRO No Stats\n4: Exit\n\n')
+    choice = input("\n1: Scan SRO (with stats)\n2: No Statistic SRO Scan\n3: Daily Statistics\n4: Exit\n\n")
 
     if choice == "1":
         scan("stat_mode_ON")
     elif choice == "2":
+        scan("stat_mode_OFF")
+    elif choice == "3":
         print(dailyStats())
         menu()
-    elif choice == "3":
-        scan("stat_mode_OFF")
     elif choice == "4":
         sys.exit(0)
     else:
@@ -59,33 +59,33 @@ def statMode(stat_mode):
 
 def scan(stat_mode):
     global sro_list
-    raw_sro = input('\nScan SRO (' + stat_mode + '):\n\n').upper()
+    raw_sro = input("\nScan SRO (" + stat_mode + "):\n\n").upper()
     raw_sro_list = split(raw_sro)
 
     #check if SRO has been scanned in using Colemak-Mod DH layout or not
     if raw_sro_list[0]+raw_sro_list[1]+raw_sro_list[2] == "RPY":
         mod_sro = raw_sro.replace('R', 'S').replace('P', 'R').replace('Y', 'O').replace('	','')
-        if mod_sro not in sro_list and stat_mode == 'stat_mode_ON':
+        if mod_sro not in sro_list and stat_mode == "stat_mode_ON":
             sro_list.append(mod_sro)
         pyperclip.copy(mod_sro)
     elif raw_sro_list[0]+raw_sro_list[1]+raw_sro_list[2] == "SRO":
-        if raw_sro not in sro_list and stat_mode == 'stat_mode_ON':
+        if raw_sro not in sro_list and stat_mode == "stat_mode_ON":
             sro_list.append(raw_sro)
         pyperclip.copy(raw_sro)
-    elif raw_sro == 'MENU':
+    elif raw_sro == "MENU":
         menu()
     elif raw_sro == "DAILY":
         print(dailyStats())
         scan(stat_mode)
-    elif raw_sro == 'MODE':
+    elif raw_sro == "MODE":
         stat_mode = statMode(stat_mode)
-    elif raw_sro == 'EXIT':
+    elif raw_sro == "EXIT":
         sys.exit(0)
     else:
         print("SRO not detected, try again.")
         scan(stat_mode)
 
-    print('\n' + pyperclip.paste() + ' copied to clipboard')
+    print("\n" + pyperclip.paste() + " copied to clipboard")
     scan(stat_mode)
 
 menu()
