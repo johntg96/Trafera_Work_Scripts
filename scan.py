800aeca7773912131338acb53115ea28d2b613fb
John Garrison <johntg@protonmail.com>
2022-01-12 20:44:19 -0600

    Delete mode added. SRO's can now be deleted from the SRO list. SRO character length validity check created. SRO's are always 10 characters (3 letters 'SRO' + 7 numbers following). Any SRO under 10 characters will flag as an error. Prevents partial scans from being added to daily SRO list (to avoid having to delete them, or mess up daily SRO count).

--- scan.py
+++ scan.py
@@ -30,16 +30,18 @@ def dailyStats():
     return "Total # of unique SRO's scanned: " + str(num_units)
 
 def menu():
-    choice = input("\n1: Scan SRO (with stats)\n2: No Statistic SRO Scan\n3: Daily Statistics\n4: Exit\n\n")
+    choice = input("\n1: Scan SRO (with stats)\n2: No Statistic SRO Scan\n3: Delete SRO from List\n4: Daily Statistics\n5: Exit\n\n")
 
     if choice == "1":
         scan("stat_mode_ON")
     elif choice == "2":
         scan("stat_mode_OFF")
     elif choice == "3":
+        scan("stat_mode_DELETE")
+    elif choice == "4":
         print(dailyStats())
         menu()
-    elif choice == "4":
+    elif choice == "5":
         sys.exit(0)
     else:
         print("Invalid choice")
@@ -48,11 +50,13 @@ def menu():
 def statMode(stat_mode):
     while True:
         print("Stat mode: " + stat_mode)
-        switch_mode = input('Type "on" or "off" to switch stat mode.\n').upper()
-        if switch_mode == "ON":
+        switch_mode = input("1: stat_mode_ON\n2: stat_mode_OFF\n3: stat_mode_DELETE\n")
+        if switch_mode == "1":
             return "stat_mode_ON"
-        elif switch_mode == "OFF":
+        elif switch_mode == "2":
             return "stat_mode_OFF"
+        elif switch_mode == "3":
+            return "stat_mode_DELETE"
         else:
             print("incorrect input")
             continue
@@ -64,28 +68,57 @@ def scan(stat_mode):
 
     #check if SRO has been scanned in using Colemak-Mod DH layout or not
     if raw_sro_list[0]+raw_sro_list[1]+raw_sro_list[2] == "RPY":
-        mod_sro = raw_sro.replace('R', 'S').replace('P', 'R').replace('Y', 'O').replace('	','')
-        if mod_sro not in sro_list and stat_mode == "stat_mode_ON":
-            sro_list.append(mod_sro)
-        pyperclip.copy(mod_sro)
+        if len(raw_sro_list) < 10:
+            print("\nIncomplete SRO scanned. Please scan again.\n")
+        else:
+            mod_sro = raw_sro.replace('R', 'S').replace('P', 'R').replace('Y', 'O').replace('	','')
+            if mod_sro not in sro_list and stat_mode == "stat_mode_ON":
+                sro_list.append(mod_sro)
+                pyperclip.copy(mod_sro)
+                print("\n" + pyperclip.paste() + " copied to clipboard")
+            elif raw_sro in sro_list and stat_mode == "stat_mode_DELETE":
+                sro_list.remove(mod_sro)
+                print(mod_sro + " deleted from daily list\n")
+            elif raw_sro not in sro_list and stat_mode == "stat_mode_DELETE":
+                print(mod_sro + " is not in daily SRO list\n")
+            else:
+                pyperclip.copy(mod_sro)
+                print("\n" + pyperclip.paste() + " copied to clipboard but not added to daily list\n")
     elif raw_sro_list[0]+raw_sro_list[1]+raw_sro_list[2] == "SRO":
-        if raw_sro not in sro_list and stat_mode == "stat_mode_ON":
-            sro_list.append(raw_sro)
-        pyperclip.copy(raw_sro)
+        if len(raw_sro_list) < 10:
+            print("\nIncomplete SRO scanned. Please scan again.\n")
+        else:
+            if raw_sro not in sro_list and stat_mode == "stat_mode_ON":
+                sro_list.append(raw_sro)
+                pyperclip.copy(raw_sro)
+                print("\n" + pyperclip.paste() + " copied to clipboard")
+            elif raw_sro in sro_list and stat_mode == "stat_mode_DELETE":
+                sro_list.remove(raw_sro)
+                print(raw_sro + " deleted from daily list\n")
+            elif raw_sro not in sro_list and stat_mode == "stat_mode_DELETE":
+                print(raw_sro + " is not in daily SRO list\n")
+            else:
+                pyperclip.copy(raw_sro)
+                print("\n" + pyperclip.paste() + " copied to clipboard but not added to daily list\n")
     elif raw_sro == "MENU":
         menu()
-    elif raw_sro == "DAILY":
+    elif raw_sro == "STATS":
         print(dailyStats())
-        scan(stat_mode)
     elif raw_sro == "MODE":
         stat_mode = statMode(stat_mode)
+    elif raw_sro == "DELETE":
+        stat_mode = "stat_mode_DELETE"
+    elif raw_sro == "TURN ON":
+        stat_mode = "stat_mode_ON"
+    elif raw_sro == "TURN OFF":
+        stat_mode = "stat_mode_OFF"
     elif raw_sro == "EXIT":
         sys.exit(0)
     else:
         print("SRO not detected, try again.")
         scan(stat_mode)
 
-    print("\n" + pyperclip.paste() + " copied to clipboard")
+    
     scan(stat_mode)
 
 menu()
