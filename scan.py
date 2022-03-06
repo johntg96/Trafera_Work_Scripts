9da7a8e89a687ce09510a1495244015aab8d456c
johntg96 <johntg@protonmail.com>
2022-03-05 20:24:54 -0600

    Removed 'sro_list' list and changed all references to 'sro_list' to 'sro_dict'. Script now fully relies on SRO dictionary as it should.

--- scan.py
+++ scan.py
@@ -19,7 +19,6 @@ print('''
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
 ''')
 
-sro_list = []
 sro_dict = {}
 
 def split(word):
@@ -29,22 +28,22 @@ def calcAvg(timeframe):
     #appends each hour of the day to this list as they are worked.
     hours = []
 
-    for hour in sro_dict:
+    for hour in sro_dict.values():
         if hour[:2] not in hours:
             hours.append(hour[:2])
 
     if timeframe == "hour":
-        hourly_avg = len(sro_dict.values()) / len(hours)
+        hourly_avg = len(sro_dict) / len(hours)
         return "Hours worked: " + str(len(hours)) + "\nHourly average: " + str(int(hourly_avg))
 
     elif timeframe == "morning":
         if '07' in hours and '12' in hours:
             morning_sros = []
 
-            for sro in sro_dict.values():
+            for sro in sro_dict:
                 morning_sros.append(sro)
 
-            morning_avg = len(sro_dict.values()) / 4.5
+            morning_avg = len(sro_dict) / 4.5
             morning_total = len(morning_sros)
             return "Morning average: " + str(int(morning_avg)) + " Morning total: " + str(morning_total)
         else:
@@ -53,24 +52,24 @@ def calcAvg(timeframe):
         if '12' in hours and '03' in hours:
             afternoon_sros = []
 
-            for sro in sro_dict.values():
+            for sro in sro_dict:
                 afternoon_sros.append(sro)
 
-            afternoon_avg = len(sro_dict.values()) / 3
+            afternoon_avg = len(sro_dict) / 3
             afternoon_total = len(afternoon_sros)
             return "Afternoon average: " + str(int(afternoon_avg)) + " Afternoon total: " + str(afternoon_total)
         else:
             return "Afternoon hours not yet worked."
     elif timeframe == "day":
         if '07' in hours and '03' in hours:
-            day_avg = len(sro_dict.values()) / 7
+            day_avg = len(sro_dict) / 7
             return "Day average " + str(int(day_avg))
         else:
             return "Full day not yet worked"
 
 def dailyStats():
     if len(sro_dict) != 0:
-        return "Total # of unique SRO's scanned: " + str(len(sro_dict.values())) + "\n" + calcAvg('hour') + "\n" + calcAvg("morning") + "\n" + calcAvg("afternoon") + "\n" + calcAvg("day")
+        return "Total # of unique SRO's scanned: " + str(len(sro_dict)) + "\n" + calcAvg('hour') + "\n" + calcAvg("morning") + "\n" + calcAvg("afternoon") + "\n" + calcAvg("day")
     else:
         return "No SRO data available"
 
@@ -107,7 +106,6 @@ def statMode(stat_mode):
             continue
 
 def scan(stat_mode):
-    global sro_list
     raw_sro = input("\nScan SRO (" + stat_mode + "):\n\n").upper()
     raw_sro_list = split(raw_sro)
 
@@ -117,18 +115,16 @@ def scan(stat_mode):
             print("\nIncomplete SRO scanned. Please scan again.\n")
         else:
             mod_sro = raw_sro.replace('R', 'S').replace('P', 'R').replace('Y', 'O').replace('	','')
-            if mod_sro not in sro_list and stat_mode == "stat_mode_ON":
-                sro_list.append(mod_sro)
+            if mod_sro not in sro_dict.keys() and stat_mode == "stat_mode_ON":
                 sro_timestamp = datetime.datetime.now()
-                sro_dict[sro_timestamp.strftime("%X")] = mod_sro
+                sro_dict[mod_sro] = sro_timestamp.strftime("%X")
                 pyperclip.copy(mod_sro)
                 print(f"\n{pyperclip.paste()} copied to clipboard")
-                print(sro_dict)
-            elif mod_sro in sro_list and stat_mode == "stat_mode_DELETE":
-                sro_list.remove(mod_sro)
-                print(mod_sro + " deleted from daily list\n")
-            elif mod_sro not in sro_list and stat_mode == "stat_mode_DELETE":
-                print(f"{mod_sro} is not in daily SRO list\n")
+            elif mod_sro in sro_dict.keys() and stat_mode == "stat_mode_DELETE":
+                del sro_dict[mod_sro]
+                print(mod_sro + " deleted\n")
+            elif mod_sro not in sro_dict.keys() and stat_mode == "stat_mode_DELETE":
+                print(f"{mod_sro} not found\n")
             else:
                 pyperclip.copy(mod_sro)
                 print(f"\n{pyperclip.paste()} copied to clipboard but not added to daily list\n")
@@ -136,15 +132,16 @@ def scan(stat_mode):
         if len(raw_sro_list) < 10:
             print("\nIncomplete SRO scanned. Please scan again.\n")
         else:
-            if raw_sro not in sro_list and stat_mode == "stat_mode_ON":
-                sro_list.append(raw_sro)
+            if raw_sro not in sro_dict.keys() and stat_mode == "stat_mode_ON":
+                sro_timestamp = datetime.datetime.now()
+                sro_dict[sro_timestamp.strftime("%X")] = raw_sro
                 pyperclip.copy(raw_sro)
                 print(f"\n{pyperclip.paste()} copied to clipboard")
-            elif raw_sro in sro_list and stat_mode == "stat_mode_DELETE":
-                sro_list.remove(raw_sro)
-                print(f"{raw_sro} deleted from daily list\n")
-            elif raw_sro not in sro_list and stat_mode == "stat_mode_DELETE":
-                print(f"{raw_sro} is not in daily SRO list\n")
+            elif raw_sro in sro_dict.keys() and stat_mode == "stat_mode_DELETE":
+                del sro_dict[raw_sro]
+                print(f"{raw_sro} deleted\n")
+            elif raw_sro not in sro_dict.keys() and stat_mode == "stat_mode_DELETE":
+                print(f"{raw_sro} not found\n")
             else:
                 pyperclip.copy(raw_sro)
                 print(f"\n{pyperclip.paste()} copied to clipboard but not added to daily list\n")
@@ -160,6 +157,8 @@ def scan(stat_mode):
         stat_mode = "stat_mode_ON"
     elif raw_sro == "TURN OFF":
         stat_mode = "stat_mode_OFF"
+    elif raw_sro == "LIST":
+        print(sro_dict)
     elif raw_sro == "EXIT":
         sys.exit(0)
     else:
